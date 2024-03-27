e94abaf94eefb1ea797b6fb42246a6dbf73cc633
mindong2 <qlsnaos12@naver.com>
2024-03-27 21:01:27 +0900

    🔍️ Metadata 추가
    SEO를 위해 metadata의 속성들을 추가 시켰습니다.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,47 @@ import { Registry as StyledComponentRegistry } from "@/styles/registry";
 import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
-  title: "KDM's Portfolio.",
-  description: "성장을 갈구하는 개발자 김동민의 포트폴리오 입니다.",
+  title: "김동민의 포트폴리오",
+  description: "성장을 갈구하는 프론트엔드 개발자 김동민의 포트폴리오 입니다.",
+  keywords: [
+    "프론트",
+    "프론트엔드",
+    "frontend",
+    "개발자",
+    "프론트엔드 개발자",
+    "웹개발자",
+    "포트폴리오",
+    "portfoilo",
+    "포폴",
+    "프론트엔드 포트폴리오",
+    "프론트엔드 개발자 포트폴리오",
+    "동민",
+    "김동민",
+    "개발자 포트폴리오",
+    "react",
+    "next",
+    "js",
+    "javascript",
+    "css",
+    "html",
+    "react-query",
+  ],
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    url: "https://dongmin-kim.vercel.app/",
+    title: "김동민의 포트폴리오",
+    description: "성장을 갈구하는 프론트엔드 개발자 김동민의 포트폴리오 입니다.",
+    siteName: "Kimdongmin's Portfolio",
+    images: [
+      {
+        url: "https://dongmin-kim.vercel.app/image/face.jpg",
+        type: "image/jpg",
+        alt: "성장을 갈구하는 프론트엔드 개발자 김동민입니다.",
+      },
+    ],
+    locale: "ko_KR",
+  },
 };
 export const viewport: Viewport = {
   initialScale: 1,
